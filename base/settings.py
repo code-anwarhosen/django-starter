019b1efeb94298f94a48dfb1bdef3f2f9d379e56
Anwar Hosen <anwarhossene@gmail.com>
2025-05-26 02:12:16 +0600

    added: auth user. user/*.html files with some styles

--- base/settings.py
+++ base/settings.py
@@ -96,9 +96,9 @@ LOGIN_URL = '/user/login/'
 
 
 # Email settings for Gmail SMTP
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anwarhossene@gmail.com'
+EMAIL_HOST_USER = '@gmail.com'
 EMAIL_HOST_PASSWORD = ''
--- user/forms.py
+++ user/forms.py
@@ -9,10 +9,29 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Enter username', 'class': 'form-control'
+        })
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'Enter email', 'class': 'form-control'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Enter password', 'class': 'form-control'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm password', 'class': 'form-control'
+        })
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(max_length=150, widget=forms.PasswordInput)
+    username = forms.CharField(max_length=150,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter username', 'class': 'form-control'})
+    )
+    password = forms.CharField(max_length=150,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter password', 'class': 'form-control'})
+    )
 
     def clean(self):
         super().clean()
